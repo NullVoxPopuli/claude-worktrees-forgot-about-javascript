f3191dac17812f84ca74ab9fd3f4723e999fe3b3
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-03-06 17:36:03 -0500

    Fix

--- index.js
+++ index.js
@@ -1,8 +1,16 @@
 import { fileURLToPath } from 'node:url';
 import { execSync } from 'node:child_process';
 
-let root = execSync(`git rev-parse --show-toplevel`).toString().trim();
-
+/**
+ * For all worktrees there exists only one .git directory
+ *
+ * When invoking this in a worktree, we get the full absolute path.
+ * But when invoking in the main directory, git returns just ".git",
+ * as if that's helpful.
+ */
+let root = 
+  execSync(`git rev-parse --git-dir`).toString().trim().split('.git')[0] 
+  || execSync(`git rev-parse --show-toplevel`).toString().trim();
 
 let aDepsLocation = import.meta.resolve('typescript');
 let aDepsPath = fileURLToPath(aDepsLocation);
